faecb3e7d3d2dc2995c33afde12e258248cb3028
Daniel Kriesten <krid78@users.noreply.github.com>
2016-04-03 23:32:16 +0200

    adds a nogo … but I really want to smell it …

--- keyboard/planck/keymaps/krid78mac.c
+++ keyboard/planck/keymaps/krid78mac.c
@@ -11,8 +11,9 @@
 #include "planck.h"
 #include "keymap_extras/keymap_german_osx.h"
 #ifdef BACKLIGHT_ENABLE
-  #include "backlight.h"
+#include "backlight.h"
 #endif
+#include "krid78pwd.h"
 
 /* Each layer gets a name for readability, which is then used in the keymap matrix below.
  * The underscores don't mean anything - you can have a layer called STUFF or any other name.
@@ -25,7 +26,7 @@
 #define _CM 3   /* bottom left key pressed */
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[_QW] = { /* Qwerty {{{
+  [_QW] = { /* Qwerty {{{
     * ,-----------------------------------------------------------------------.
     * | Esc | q   | w   | e   | r   | t   | z   | u   | i   | o   | p   |  ü  |
     * |-----------------------------------------------------------------------|
@@ -37,13 +38,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * `-----------------------------------------------------------------------'
     * Ent is tap-combined with L-Shift (R-Shift is not available)
     */
-  {KC_ESC,  DE_Q,    DE_W,    DE_E,    DE_R,    DE_T,    DE_Z,    DE_U,    DE_I,    DE_O,    DE_P,    DE_UE},
-  {KC_TAB,  DE_A,    DE_S,    DE_D,    DE_F,    DE_G,    DE_H,    DE_J,    DE_K,    DE_L,    DE_OE,   DE_AE},
-  {KC_LSFT, DE_Y,    DE_X,    DE_C,    DE_V,    DE_B,    DE_N,    DE_M,    DE_COMM, DE_DOT,  DE_MINS, SFT_T(KC_ENT)},
-  {MO(_CM), KC_LCTL, KC_LALT, KC_LGUI, MO(_LW), KC_SPC,  KC_SPC,  MO(_RS), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
-  /*------|--------|--------|--------|-Lower--|------Space------|-Raise--|--------|--------|--------|--------*/
-}, /* }}} */
-[_LW] = { /* LOWER (Left Modifier, blue) {{{
+    {KC_ESC,  DE_OSX_Q,    DE_OSX_W,    DE_OSX_E,    DE_OSX_R,    DE_OSX_T,    DE_OSX_Z,    DE_OSX_U,    DE_OSX_I,    DE_OSX_O,    DE_OSX_P,    DE_OSX_UE},
+    {KC_TAB,  DE_OSX_A,    DE_OSX_S,    DE_OSX_D,    DE_OSX_F,    DE_OSX_G,    DE_OSX_H,    DE_OSX_J,    DE_OSX_K,    DE_OSX_L,    DE_OSX_OE,   DE_OSX_AE},
+    {KC_LSFT, DE_OSX_Y,    DE_OSX_X,    DE_OSX_C,    DE_OSX_V,    DE_OSX_B,    DE_OSX_N,    DE_OSX_M,    DE_OSX_COMM, DE_OSX_DOT,  DE_OSX_MINS, SFT_T(KC_ENT)},
+    {MO(_CM), KC_LCTL, KC_LALT, KC_LGUI, MO(_LW), KC_SPC,  KC_SPC,  MO(_RS), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
+    /*------|--------|--------|--------|-Lower--|------Space------|-Raise--|--------|--------|--------|--------*/
+  }, /* }}} */
+  [_LW] = { /* LOWER (Left Modifier, blue) {{{
     * ,-----------------------------------------------------------------------.
     * | 1   | 2   | 3   | 4   | 5   | 6   | 7   | 8   | 9   | 0   | ß   | Bspc|
     * |-----------------------------------------------------------------------|
@@ -54,13 +55,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * |     | RCtl| RAlt| RGui|Lower|   Space   |Raise| Mute| Vol-| Vol+| Play|
     * `-----------------------------------------------------------------------'
     */
-  {DE_1,    DE_2,    DE_3,    DE_4,    DE_5,    DE_6,    DE_7,    DE_8,    DE_9,    DE_0,    DE_SS,   KC_BSPC},
-  {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, DE_LBRC, DE_RBRC, DE_HASH, DE_PLUS},
-  {KC_TRNS, DE_LESS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, DE_PIPE, KC_TRNS},
-  {KC_TRNS, KC_RCTL,  KC_RALT, KC_RGUI, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY}
-  /*------|--------|--------|--------|-Lower--|------Space------|-Raise--|--------|--------|--------|--------*/
-}, /* }}} */
-[_RS] = { /* RAISE (Right Modifier, orange) {{{
+    {DE_OSX_1,    DE_OSX_2,    DE_OSX_3,    DE_OSX_4,    DE_OSX_5,    DE_OSX_6,    DE_OSX_7,    DE_OSX_8,    DE_OSX_9,    DE_OSX_0,    DE_OSX_SS,   KC_BSPC},
+    {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, DE_OSX_LBRC, DE_OSX_RBRC, DE_OSX_HASH, DE_OSX_PLUS},
+    {KC_TRNS, DE_OSX_LESS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, DE_OSX_PIPE, KC_TRNS},
+    {KC_TRNS, KC_RCTL,  KC_RALT, KC_RGUI, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY}
+    /*------|--------|--------|--------|-Lower--|------Space------|-Raise--|--------|--------|--------|--------*/
+  }, /* }}} */
+  [_RS] = { /* RAISE (Right Modifier, orange) {{{
     * ,-----------------------------------------------------------------------.
     * | !   | "   | §   | $   | %   | &   | /   | (   | )   | =   | ?   | ´   |
     * |-----------------------------------------------------------------------|
@@ -70,18 +71,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * |-----------------------------------------------------------------------|
     * |     |     |     |     |     |           |     |     |     |     |     |
     * `-----------------------------------------------------------------------'
+    * " - just a closing ""
     */
-  {DE_EXLM, DE_DQOT, DE_PARA, DE_DLR,  DE_PERC, DE_AMPR, DE_SLSH, DE_LPRN, DE_RPRN, DE_EQL,  DE_QST,  DE_ACUT},
-  {DE_CIRC, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, DE_LCBR, DE_RCBR, DE_QUOT, DE_ASTR},
-  {KC_TRNS, DE_MORE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, DE_BSLS, KC_TRNS},
-  {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS}
-  /*------|--------|--------|--------|-Lower--|------Space------|-Raise--|--------|--------|--------|--------*/
+    {DE_OSX_EXLM, DE_OSX_DQOT, DE_OSX_PARA, DE_OSX_DLR,  DE_OSX_PERC, DE_OSX_AMPR, DE_OSX_SLSH, DE_OSX_LPRN, DE_OSX_RPRN, DE_OSX_EQL,  DE_OSX_QST,  DE_OSX_ACUT},
+    {DE_OSX_CIRC, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, DE_OSX_LCBR, DE_OSX_RCBR, DE_OSX_QUOT, DE_OSX_ASTR},
+    {KC_TRNS, DE_OSX_MORE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, DE_OSX_BSLS, KC_TRNS},
+    {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS}
+    /*------|--------|--------|--------|-Lower--|------Space------|-Raise--|--------|--------|--------|--------*/
 
 
-}, /* }}} */
-[_CM] = { /* Control Layer {{{
+  }, /* }}} */
+  [_CM] = { /* Control Layer {{{
     * ,-----------------------------------------------------------------------.
-    * |RESET|     |     |     |     |     |     |     |     |     |     | `   |
+    * |RESET| M(0)| M(1)| M(2)|     |     |     |     |     |     |     | `   |
     * |-----------------------------------------------------------------------|
     * | F1  | F2  | F3  | F4  | F5  | F6  | F7  | F8  | F9  | F10 | F11 | F12 |
     * |-----------------------------------------------------------------------|
@@ -90,12 +92,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * |     |     |     |     |     |           |     |     | Blt-| Blt+|     |
     * `-----------------------------------------------------------------------'
     */
-  {  RESET, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, DE_ACUT},
-  {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS},
-  {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS},
-  {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, M(0),    M(1),    KC_TRNS}
-  /*------|--------|--------|--------|-Lower--|------Space------|-Raise--|--------|--------|--------|--------*/
-} /* }}} */
+    {RESET,   M(0),    M(1),    M(2),    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, DE_OSX_ACUT},
+    {KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12},
+    {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS},
+    {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, BL_TOGG, BL_DEC,  BL_INC,  KC_TRNS}
+    /*------|--------|--------|--------|-Lower--|------Space------|-Raise--|--------|--------|--------|--------*/
+  } /* }}} */
 };
 
 const uint16_t PROGMEM fn_actions[] = {
@@ -105,27 +107,40 @@ const uint16_t PROGMEM fn_actions[] = {
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 {
   // MACRODOWN only works in this function
-      switch(id) {
-        case 0:
-          if (record->event.pressed) {
-            register_code(KC_RSFT);
-            #ifdef BACKLIGHT_ENABLE
-              backlight_decrease();
-            #endif
-          } else {
-            unregister_code(KC_RSFT);
-          }
-        break;
-        case 1:
-          if (record->event.pressed) {
-            register_code(KC_RSFT);
-            #ifdef BACKLIGHT_ENABLE
-              backlight_increase();
-            #endif
-          } else {
-            unregister_code(KC_RSFT);
-          }
-        break;
+  switch(id) {
+    case 0: // this would trigger when you hit a key mapped as M(0)
+      if (record->event.pressed) {
+#ifdef MAC_PWD
+        return MAC_PWD;
+#else
+        return MACRO( I(255), T(H), T(E), T(L), T(L), W(255), T(O), END  );
+#endif
       }
-    return MACRO_NONE;
+      break;
+    case 1: // this would trigger when you hit a key mapped as M(1)
+      if (record->event.pressed) {
+#ifdef TUC_PWD
+        return TUC_PWD;
+#else
+        return MACRO( I(255), T(H), T(E), T(L), T(L), W(255), T(O), END  );
+#endif
+      }
+      break;
+    case 2: // this would trigger when you hit a key mapped as M(2)
+      if (record->event.pressed) {
+#ifdef ONE_PWD
+        return ONE_PWD;
+#else
+        return MACRO( I(255), T(H), T(E), T(L), T(L), W(255), T(O), END  );
+#endif
+      }
+      break;
+  }
+  return MACRO_NONE;
 };
+
+/*
+ * vim: ts=2:sw=2:sts=2:tw=80:fileformat=unix
+ * vim: comments& comments+=b\:* formatoptions& formatoptions+=or
+ */
+
