d70a69137f58c244276bab883903db94fd3dcb67
Daniel Kriesten <krid78@users.noreply.github.com>
2016-04-11 08:28:28 +0200

    adds some keyboard MACROs. SCNR

--- keyboard/kc60/keymaps/krid78.c
+++ keyboard/kc60/keymaps/krid78.c
@@ -10,6 +10,7 @@
 #ifdef BACKLIGHT_ENABLE
 #include "backlight.h"
 #endif
+#include "krid78pwd.h"
 
 const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
     /* Keymap 0: Default Layer
@@ -27,10 +28,10 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
      * FN7 - is Tab or switches to Layer2
      */
     KEYMAP_ANSI( //0
-        KC_ESC,  DE_1,    DE_2,    DE_3,   DE_4,    DE_5,    DE_6,  DE_7,     DE_8,    DE_9,    DE_0,    DE_SS,   DE_ACUT, KC_BSPC, \
-        F(0),    DE_Q,    DE_W,    DE_E,   DE_R,    DE_T,    DE_Z,  DE_U,     DE_I,    DE_O,    DE_P,    DE_UE,   DE_PLUS, DE_HASH, \
-        MO(1),   DE_A,    DE_S,    DE_D,   DE_F,    DE_G,    DE_H,  DE_J,     DE_K,    DE_L,    DE_OE,   DE_AE,   KC_ENT,  \
-        KC_LSFT, DE_Y,    DE_X,    DE_C,   DE_V,    DE_B,    DE_N,  DE_M,     DE_COMM, DE_DOT,  DE_MINS, KC_RSFT, \
+        KC_ESC,  DE_OSX_1,    DE_OSX_2,    DE_OSX_3,   DE_OSX_4,    DE_OSX_5,    DE_OSX_6,  DE_OSX_7,     DE_OSX_8,    DE_OSX_9,    DE_OSX_0,    DE_OSX_SS,   DE_OSX_ACUT, KC_BSPC, \
+        F(0),    DE_OSX_Q,    DE_OSX_W,    DE_OSX_E,   DE_OSX_R,    DE_OSX_T,    DE_OSX_Z,  DE_OSX_U,     DE_OSX_I,    DE_OSX_O,    DE_OSX_P,    DE_OSX_UE,   DE_OSX_PLUS, DE_OSX_HASH, \
+        MO(1),   DE_OSX_A,    DE_OSX_S,    DE_OSX_D,   DE_OSX_F,    DE_OSX_G,    DE_OSX_H,  DE_OSX_J,     DE_OSX_K,    DE_OSX_L,    DE_OSX_OE,   DE_OSX_AE,   KC_ENT,  \
+        KC_LSFT, DE_OSX_Y,    DE_OSX_X,    DE_OSX_C,   DE_OSX_V,    DE_OSX_B,    DE_OSX_N,  DE_OSX_M,     DE_OSX_COMM, DE_OSX_DOT,  DE_OSX_MINS, KC_RSFT, \
         KC_LCTL, KC_LALT, KC_LGUI,                  KC_SPC,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT),
     /* Keymap 1: Fn Layer
      * ,-----------------------------------------------------------.
@@ -46,14 +47,14 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
      * `-----------------------------------------------------------'
      */
     KEYMAP_ANSI( //1
-        DE_CIRC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,    KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  \
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, DE_LCBR, DE_RCBR, DE_PIPE, \
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN,  KC_UP,   KC_RGHT, DE_LBRC, DE_RBRC, KC_TRNS, \
-        KC_TRNS, DE_LESS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
+        DE_OSX_CIRC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,    KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  \
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, DE_OSX_LCBR, DE_OSX_RCBR, DE_OSX_PIPE, \
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN,  KC_UP,   KC_RGHT, DE_OSX_LBRC, DE_OSX_RBRC, KC_TRNS, \
+        KC_TRNS, DE_OSX_LESS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
         KC_RCTL, KC_RALT, KC_RGUI, KC_SPC,  KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY),
     /* Keymap 2: Fn Layer
      * ,-----------------------------------------------------------.
-     * |RESET|   |   |   |   |   |   |   |   |   |   |   |   | Pwr |
+     * |RESET| M0| M1| M2|   |   |   |   |   |   |   |   |   | Pwr |
      * |-----------------------------------------------------------|
      * |     |   |   |   |   |   |   |   |   |   |   |   |   |     |
      * |-----------------------------------------------------------|
@@ -65,39 +66,46 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
      * `-----------------------------------------------------------'
      */
     KEYMAP_ANSI( //2
-        RESET,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PWR,  \
+        RESET,   M(0),    M(1),    M(2), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PWR,  \
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
-        KC_LCTL, KC_LALT, KC_LGUI,                   KC_SPC,                    F(1),    M(0),    M(1),    KC_TRNS),
+        KC_LCTL, KC_LALT, KC_LGUI,                   KC_SPC,                    BL_TOGG, BL_DEC,  BL_INC,  KC_TRNS),
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 {
   // MACRODOWN only works in this function
-      switch(id) {
-        case 0:
-          if (record->event.pressed) {
-            register_code(KC_RSFT);
-            #ifdef BACKLIGHT_ENABLE
-              backlight_decrease();
-            #endif
-          } else {
-            unregister_code(KC_RSFT);
-          }
-        break;
-        case 1:
-          if (record->event.pressed) {
-            register_code(KC_RSFT);
-            #ifdef BACKLIGHT_ENABLE
-              backlight_increase();
-            #endif
-          } else {
-            unregister_code(KC_RSFT);
-          }
-        break;
+  switch(id) {
+    case 0: // this would trigger when you hit a key mapped as M(0)
+      if (record->event.pressed) {
+#ifdef MAC_PWD
+        return MAC_PWD;
+#else
+        return MACRO( I(255), T(H), T(E), T(L), T(L), W(255), T(O), END  );
+#endif
       }
-    return MACRO_NONE;
+      break;
+    case 1: // this would trigger when you hit a key mapped as M(1)
+      if (record->event.pressed) {
+#ifdef TUC_PWD
+        return TUC_PWD;
+#else
+        return MACRO( I(255), T(H), T(E), T(L), T(L), W(255), T(O), END  );
+#endif
+      }
+      break;
+    case 2: // this would trigger when you hit a key mapped as M(2)
+      if (record->event.pressed) {
+#ifdef ONE_PWD
+        return ONE_PWD;
+#else
+        return MACRO( I(255), T(H), T(E), T(L), T(L), W(255), T(O), END  );
+#endif
+      }
+      break;
+  }
+  return MACRO_NONE;
 };
 
 /*
@@ -105,8 +113,8 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
  */
 const uint16_t fn_actions[] PROGMEM = {
     [0] = ACTION_LAYER_TAP_KEY(2, KC_TAB), // Tab or FN2
-    [1] = ACTION_BACKLIGHT_TOGGLE(),       // Backlight
-    /* [2] = ACTION_BACKLIGHT_DECREASE(),     // Backlight- */
+    /* [1] = ACTION_BACKLIGHT_TOGGLE(),       // Backlight */
+    /* [2] = ACTION_BACKLIGHT_DE_OSXCREASE(),     // Backlight- */
     /* [3] = ACTION_BACKLIGHT_INCREASE(),     // Backlight+ */
 };
 
